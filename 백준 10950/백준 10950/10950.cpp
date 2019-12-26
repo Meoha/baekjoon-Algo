9f78a95de6660180fcb35aea3a4e9ce95ce699e6
조대하 <dh9306@ajou.ac.kr>
2019-12-26 20:46:44 +0900

    백준 10950 A+B-3 알고리즘 완료

---
+++ 백준 10950/백준 10950/10950.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int N;
+	int a, b;
+
+	cin >> N;
+
+	for (int n = 0; n < N; n++) {
+
+		cin >> a >> b;
+
+		cout << a + b << '\n';
+
+	}
+
+	return 0;
+}
