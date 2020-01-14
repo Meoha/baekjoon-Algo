0765cbdcb910d0814fdde7b1364662029d187713
조대하 <dh9306@ajou.ac.kr>
2020-01-14 18:27:13 +0900

    백준 4153 직각삼각형 알고리즘 완료

---
+++ 백준 4153/백준 4153/4153.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b, c;
+
+	while (cin >> a >> b >> c) {
+
+		if (a == 0 && b == 0 && c == 0) {
+
+			break;
+		}
+
+		if (a >= b) {
+
+			if (a >= c) {
+
+				if (a * a == b * b + c * c) {
+
+					cout << "right" << '\n';
+
+				}
+				else {
+
+					cout << "wrong" << '\n';
+				}
+
+			}
+			else {
+
+				if (c * c == b * b + a * a) {
+
+					cout << "right" << '\n';
+
+				}
+				else {
+
+					cout << "wrong" << '\n';
+				}
+
+			}
+
+		}
+		else {
+
+			if (b >= c) {
+
+				if (b * b == a * a + c * c) {
+
+					cout << "right" << '\n';
+
+				}
+				else {
+
+					cout << "wrong" << '\n';
+				}
+
+			}
+			else {
+
+				if (c * c == b * b + a * a) {
+
+					cout << "right" << '\n';
+
+				}
+				else {
+
+					cout << "wrong" << '\n';
+				}
+
+			}
+			
+		}
+
+	}
+
+	return 0;
+}
