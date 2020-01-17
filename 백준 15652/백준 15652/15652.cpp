d05de51e59bc0c74d736d732eb422644aa34e0b8
조대하 <dh9306@ajou.ac.kr>
2020-01-17 17:28:14 +0900

    백준 15652 N과 M (4) 알고리즘 완료

---
+++ 백준 15652/백준 15652/15652.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+
+void sol(vector<int> v, int last) {
+
+	if (v.size() == M) {
+
+		for (int i = 0; i < M; i++) {
+
+			cout << v[i] << ' ';
+
+		}
+
+		cout << '\n';
+
+		return;
+	}
+	else {
+
+		for (int i = last; i <= N; i++) {
+
+			if (i >= last) {
+
+				v.push_back(i);
+
+				sol(v, i);
+
+				v.pop_back();
+
+			}
+
+		}
+
+	}
+
+	return;
+}
+
+int main() {
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N >> M;
+
+	vector<int> v;
+
+	sol(v, 1);
+
+	return 0;
+}
