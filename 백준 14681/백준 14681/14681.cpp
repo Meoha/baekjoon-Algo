16646ef72d1c9d4bd72924a354b5d4667bac61bc
조대하 <dh9306@ajou.ac.kr>
2020-08-07 18:38:31 +0900

    백준 14681 사분면 고르기 알고리즘 완료

---
+++ 백준 14681/백준 14681/14681.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int x, y = 0;
+
+	cin >> x >> y;
+
+	if (x > 0) {
+
+		if (y > 0) {
+
+			cout << "1" << '\n';
+
+		}
+		else {
+
+			cout << "4" << '\n';
+
+		}
+
+	}
+	else {
+
+		if (y > 0) {
+
+			cout << "2" << '\n';
+
+		}
+		else {
+
+			cout << "3" << '\n';
+
+		}
+
+	}
+
+	return 0;
+}
